435006b99c9c9877cd11bf321cdddd4f57a075a1
qwpyyx <qiuwenqi788@gmail.com>
2024-10-30 21:36:45 +0800

    dd

--- src/run_uie_lora.py
+++ src/run_uie_lora.py
@@ -21,7 +21,6 @@ Fine-tuning the library models for sequence to sequence.
 # You can also adapt this script on your own sequence to sequence task. Pointers for this are left as comments.
 import warnings
 import deepspeed
-deepspeed.ops.op_builder.CPUAdamBuilder().load()
 import logging
 import os
 import sys
